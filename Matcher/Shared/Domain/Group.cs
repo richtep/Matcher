60d6897313a73eb111ced011f8e8453d51deabfc
Pavel Richter <pavel.richter.cista@gmail.com>
2022-10-19 17:44:21 +0200

    commented code removed

--- Matcher/Shared/Domain/Group.cs
+++ Matcher/Shared/Domain/Group.cs
@@ -5,29 +5,6 @@ namespace Matcher.Shared.Domain;
 
 public class Group : Entity
 {
- 
     [Required]
     public virtual Game Game {get; set;}
-
-    /*
-    public virtual string Name { get; set; }
-        
-    public static readonly Initial Reference = new();
-
-    public class Holder : Holder<Group>
-    {
-        public Group Home { get; set; } = new() { Name = "Home" };
-
-        public Group Work { get; set; } = new() { Name = "Work" };
-    }
-
-    public class Initial : Initial<Holder, Group>
-    {
-        public Initial() : base(
-            h => h.Home,
-            h => h.Work)
-        {
-        }
-    }
-    */
 }
