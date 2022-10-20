3e0fcb85761ea4953e47afe072bbdea19a360bb6
Pavel Richter <pavel.richter.cista@gmail.com>
2022-10-20 21:19:28 +0200

    refactored

--- Matcher/Shared/Domain/Game.cs
+++ Matcher/Shared/Domain/Game.cs
@@ -9,9 +9,7 @@ public class Game : Entity
     public virtual string Name { get; set; }
     [Required]
     public virtual User User { get; set; }
-
-    public virtual bool Active { get; set; } = true;
-
+        
     [Required]
     public virtual string Password { get; set; }
 }
