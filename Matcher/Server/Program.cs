febeff91d5119786d190a0ba92e624fb6fe102ec
Pavel Richter <pavel.richter.cista@gmail.com>
2024-10-18 20:24:24 +0200

    make database internal

--- Matcher/Server/Program.cs
+++ Matcher/Server/Program.cs
@@ -8,7 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
-builder.Services.AddEntitySystemServerFluent<ItemService, Item>(PostgreSQLConfiguration.Standard.ConnectionString("server=130.162.216.255;Port=8083;Database=dbMatcher;User Id=admin;Password=Veverka378378;MaxPoolSize=100;Timeout=15;CommandTimeout=300").Dialect<PostgreSQL83Dialect>());
+
+//builder.Services.AddEntitySystemServerFluent<ItemService, Item>(PostgreSQLConfiguration.Standard.ConnectionString("server=130.162.216.255;Port=8083;Database=dbMatcher;User Id=admin;Password=Veverka378378;MaxPoolSize=100;Timeout=15;CommandTimeout=300").Dialect<PostgreSQL83Dialect>());
+
+builder.Services.AddEntitySystemServerFluent<ItemService, Item>(PostgreSQLConfiguration.Standard.ConnectionString("server=Database;Port=5432;Database=dbMatcher;User Id=admin;Password=Veverka378378;MaxPoolSize=100;Timeout=15;CommandTimeout=300").Dialect<PostgreSQL83Dialect>());
+
 
 /*builder.Services.AddEntitySystemServerFluent<ItemService, Item>
 (i => i.EnsureExist(Group.Reference, g => g.Name));
